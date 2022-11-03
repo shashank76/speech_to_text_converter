fb1dcd54f819601cb32b8ab8db1aecaeee07be55
Shashank Tripathi <shashankmtripathi76@gmail.com>
2022-11-03 12:32:08 -0500

    Create speech to text converter using python librarires

---
+++ speech_to_text.py
@@ -0,0 +1,7 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+mic = sr.Microphone()
+print(sr.Microphone.list_microphone_names())
+with mic as source:
+    audio = r.listen(source)
+    print(r.recognize_google(audio))
